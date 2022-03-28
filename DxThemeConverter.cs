d249aed81b3b8ed8d1c555f69776c047dbaf220d
Troshkin Andrew <a.troshkin@npktrans.ru>
2022-03-28 15:09:49 +0300

    Добавил переменные *-о, чтобы можно было использовать цвета с прозрачностью.

--- DxThemeConverter.cs
+++ DxThemeConverter.cs
@@ -35,14 +35,17 @@ public static class DxThemeConverter
     foreach (var themeColorRule in themeColorRules)
     {
       var cssVarName = colorClassesDictionary[themeColorRule.SelectorText];
+      var cssVarName2 = colorClassesDictionary[themeColorRule.SelectorText] + "-o"; //Это для использования с прозрачностью
       //Беру значение цвета
       var varValue = themeColorRule.Style.Color;
       //Если значение цвета пустое, значит это класс для фонового цвета
       // и надо брать свойство не color, а background-color(или fill) они всегда в теме рядом
       if (string.IsNullOrEmpty(varValue)) varValue = themeColorRule.Style.BackgroundColor;
       if (string.IsNullOrEmpty(varValue)) varValue = themeColorRule.Style.BorderColor;
-      //Хотел в одну строку, но студия посоветовала так
+
+      var varValue2 = varValue.Replace("rgb(", "").Replace(")", ""); //Эта переменная, чтобы использовать прозрачность
       result.Append("  --").Append(cssVarName).Append(": ").Append(varValue).AppendLine(";");
+      result.Append("  --").Append(cssVarName2).Append(": ").Append(varValue2).AppendLine(";");
     }
     result.AppendLine("}");
     return result.ToString();
